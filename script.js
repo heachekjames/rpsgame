6cbaa097256f674904e54e9278f84797e6da1221
heachekjames <gabiasdsda@gmail.com>
2022-08-08 22:21:46 +0300

    quality of life changes

---
+++ script.js
@@ -0,0 +1,51 @@
+
+function getComputerChoice(){
+let roll = Math.floor(Math.random() * 3);
+let result;
+
+if(roll == 0){
+    result = 'Scissors';
+}else if(roll == 1){
+    result = 'Paper';
+}else if(roll == 2){
+    result = 'Rock';
+}
+
+return result;
+
+}
+
+let playerSelection = (prompt("enter r p s"));
+
+function playRound(){
+let playerSelectionLowercase = playerSelection.toLowerCase();
+let getComputerChoiceLowercase = getComputerChoice().toLowerCase();
+if(playerSelectionLowercase == getComputerChoiceLowercase){
+    console.log("draw");
+}else if(playerSelectionLowercase == "rock" && getComputerChoiceLowercase == "scissors"){
+    console.log("winner");
+
+}else if(playerSelectionLowercase == "paper" && getComputerChoiceLowercase == "rock"){
+    console.log("winner");
+
+}else if(playerSelectionLowercase == "scissors" && getComputerChoiceLowercase == "paper"){
+    console.log("winner");
+
+}else{
+    console.log("loser");
+}
+
+console.log("computer rolled " + getComputerChoiceLowercase);
+}
+
+function game(){
+    for(let i=0; i<5;i++)
+    {
+        playerSelection = (prompt("enter r p s"));
+            playRound();
+    }
+}
+
+game();
+
+
